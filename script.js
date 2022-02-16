c64ced91f6f8ff9324bb6b5a35fb71afba01d634
MdMehedi-Hasan <mh19115@gmail.com>
2022-02-17 03:28:29 +0600

    function added

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
+function collectId(idName){
+    const input = document.getElementById(idName);
+    const inputValue = parseFloat(input.value);
+    input.value='';
+    return inputValue;
+}
+function collectingInnerText(idName){
+    const text = document.getElementById(idName);
+    return text;
+}
 document.getElementById('calculate-btn').addEventListener('click',function(){
-    const income = document.getElementById('income-field');
-    const incomeValue = parseFloat(income.value);
+    const incomeField = collectId('income-field');
+    const foodField = collectId('food-field');
+    const rentField = collectId('rent-field');
+    const clothesField = collectId('clothes-field');
 
-    const food = document.getElementById('food-field');
-    const foodValue = parseFloat(food.value);
-
-    const rent = document.getElementById('rent-field');
-    const rentValue = parseFloat(rent.value);
-
-    const clothes = document.getElementById('clothes-field');
-    const clothesValue = parseFloat(clothes.value);
-
-    const totalExpenses = document.getElementById('total-expenses');
-    const totalExpensesValue = parseFloat(totalExpenses.innerText);
-
-    const balance = document.getElementById('balance');
-    const balanceValue = parseFloat(balance.innerText);
-
-    const savingAamount = document.getElementById('savings-amount');
-    const savingAmountValue = parseFloat(savingAamount.innerText);
-
-    const remainingBalance = document.getElementById('remaining-balance');
-    const remainingBalanceValue = parseFloat(remainingBalance.innerText);
+    const expenses = collectingInnerText('total-expenses');
+    const balance = collectingInnerText('balance');
+    const saving = collectingInnerText('savings-amount');
+    const remaining = collectingInnerText('remaining-balance');
     //input id's has been taken 
 
+    const addingExpenses = foodField+rentField+clothesField;
+    expenses.innerText=addingExpenses;
 
-
-    // clear input
-    income.value = '';
+    const subtracting = incomeField-addingExpenses;
+    balance.innerText = subtracting;
     
 })
