da01337ce3971ea37948cb2209eb16a5f2a1b35a
MdMehedi-Hasan <mh19115@gmail.com>
2022-02-17 03:52:40 +0600

    project almost complete

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function collectId(idName){
     const input = document.getElementById(idName);
     const inputValue = parseFloat(input.value);
-    input.value='';
     return inputValue;
 }
-function collectingInnerText(idName){
+function collectingId(idName){
     const text = document.getElementById(idName);
     return text;
 }
@@ -13,17 +12,22 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
     const foodField = collectId('food-field');
     const rentField = collectId('rent-field');
     const clothesField = collectId('clothes-field');
-
-    const expenses = collectingInnerText('total-expenses');
-    const balance = collectingInnerText('balance');
-    const saving = collectingInnerText('savings-amount');
-    const remaining = collectingInnerText('remaining-balance');
+    const expenses = collectingId('total-expenses');
+    const balance = collectingId('balance');
     //input id's has been taken 
-
     const addingExpenses = foodField+rentField+clothesField;
     expenses.innerText=addingExpenses;
-
     const subtracting = incomeField-addingExpenses;
     balance.innerText = subtracting;
-    
+})
+
+document.getElementById('save-btn').addEventListener('click',function(){
+    const saving = collectingId('savings-amount');
+    const remaining = collectingId('remaining-balance');
+    const saveField = collectId('save-field')
+    const incomeField = collectId('income-field');
+    const balance = collectingId('balance');
+    const calcPercantage = (incomeField*saveField)/100;
+    saving.innerText = calcPercantage;
+    remaining.innerText = parseFloat(balance.innerText) - parseFloat(saving.innerText);
 })
