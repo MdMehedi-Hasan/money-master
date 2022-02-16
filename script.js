6b570a21405c654b18ca7aa562f6c0c7c0e6dc60
MdMehedi-Hasan <mh19115@gmail.com>
2022-02-17 02:07:30 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/* 
+income input = income-field
+food input = food-field
+rent input = rent-field
+clothes input = clothes-field
+save input = save-field
+
+calculate button = calculate-btn
+save button = save-btn
+ */
+/* document.getElementById('save-btn').addEventListener('click',function(){
+    const incomeInput = document.getElementById('save-field');
+    const incomeInputValue = incomeInput.value;
+    incomeInput.value = '';
+    console.log(incomeInputValue);
+}) */
